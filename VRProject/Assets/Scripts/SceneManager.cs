8de8122e8ef81841c4085c45b3153d2ef173eaa8
Christian <c.schawe@web.de>
2019-11-28 14:58:03 +0100

    initial commit to add the project, campsite scene and first look of the sorting scene

---
+++ VRProject/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnitySceneManager = UnityEngine.SceneManagement.SceneManager;
+
+public class SceneManager : MonoBehaviour
+{
+    public KeyCode sceneCampsite;
+    public KeyCode sceneFishing;
+    public KeyCode sceneSorting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sceneCampsite = KeyCode.Alpha1;
+        sceneFishing = KeyCode.Alpha2;
+        sceneSorting = KeyCode.Alpha3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(sceneCampsite))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Campsite");
+        } else if( Input.GetKeyDown(sceneFishing))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Fishing");
+        } else if(Input.GetKeyDown(sceneSorting))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Sorting");
+        }
+    }
+}
