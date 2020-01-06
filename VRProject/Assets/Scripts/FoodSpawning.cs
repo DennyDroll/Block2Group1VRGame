413efee55334f1c08c635f7fcb7bb278a6bdb5db
Christian <c.schawe@web.de>
2020-01-06 09:57:36 +0100

    Fruit picking

---
+++ VRProject/Assets/Scripts/FoodSpawning.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawning : MonoBehaviour
+{
+    public GameObject fruit;
+    public Vector3[] spawnPoints;
+    public int amount = 3;
+    private int whereToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        whereToSpawn = Random.Range(0,spawnPoints.Length);
+        for(int i = 0; i <= amount; i++)
+        {
+            Instantiate(fruit,spawnPoints[whereToSpawn],Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
